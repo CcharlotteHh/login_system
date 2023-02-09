281b8601fda99b10863296defa3dd0ddfe258ceb
CcharlotteHh <210547@student.glu.nl>
2023-02-09 13:56:52 +0100

    form works and succses works

--- src/Login.js
+++ src/Login.js
@@ -9,7 +9,7 @@ export default function Login() {
   const [errMsg, setErrMsg] = useState("");
   const [success, setSuccess] = useState(false);
 
-  function login(e) {
+  function handleLogin(e) {
     e.preventDefault();
     console.log(username, password);
     setUsername("");
@@ -17,32 +17,53 @@ export default function Login() {
     setSuccess(true);
   }
   return (
-    <form className="loginForm" onSubmit={login}>
-      <label htmlFor="username">Username</label>
-      <input
-        className="loginInput"
-        placeholder="username"
-        id="username"
-        autoComplete="false"
-        ref={userRef}
-        type="text"
-        value={username}
-        onChange={(e) => {
-          setUsername(e.target.value);
-        }}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        className="loginInput"
-        placeholder="password"
-        id="password"
-        type="password"
-        value={password}
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-      <button>Log in!</button>
-    </form>
+    <>
+    {success ? (
+        <section>
+            <h1>You are logged in!</h1>
+            <br />
+            <p>
+                <a href="#">Go to Home</a>
+            </p>
+        </section>
+    ) : (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <form className="loginForm" onSubmit={handleLogin}>
+        <label htmlFor="username">Username</label>
+        <input
+          className="loginInput"
+          placeholder="username"
+          id="username"
+          autoComplete="false"
+          ref={userRef}
+          type="text"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          className="loginInput"
+          placeholder="password"
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button>Log in!</button>
+      </form>
+    </section>
+     )}
+     </>
   );
 }
