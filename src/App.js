e598b791e79dc28f36d41dd6619ce08c21de0a93
CcharlotteHh <210547@student.glu.nl>
2023-02-10 14:15:41 +0100

    alsmost done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Login from './Login';
-
+import Home from './Home';
+import { Route, Routes } from "react-router-dom"
 function App() {
   return (
-    <main className="App">
-      <Login></Login>
-    </main>
+   
+       <Routes>
+      <Route path="home" element={<Home />} />
+      <Route path="/" element={<Login />} />
+      </Routes>
+
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,69 +1,83 @@
 import { useRef, useState, useEffect, useContext } from "react";
-
+import AuthContext from "./context/AuthProvider";
+import axios from "./api/axios";
+import { Links ,Link } from "react-router-dom";
+ 
 export default function Login() {
+  
   const userRef = useRef();
   const errRef = useRef();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
   const [errMsg, setErrMsg] = useState("");
   const [success, setSuccess] = useState(false);
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
 
-  function handleLogin(e) {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    console.log(username, password);
-    setUsername("");
-    setPassword("");
-    setSuccess(true);
+    console.log(user, pwd);
+    try {
+    //    const response = await axios.post(LOGIN_URL, JSON.stringify({})) 
+      setUser("");
+      setPwd("");
+      setSuccess(true);
+    } catch (err) {}
   }
   return (
     <>
-    {success ? (
+      {success ? (
         <section>
-            <h1>You are logged in!</h1>
-            <br />
-            <p>
-                <a href="#">Go to Home</a>
-            </p>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+          <Link to="/">Home</Link>
+        
+          </p>
         </section>
-    ) : (
-    <section>
-      <p
-        ref={errRef}
-        className={errMsg ? "errmsg" : "offscreen"}
-        aria-live="assertive"
-      >
-        {errMsg}
-      </p>
-      <form className="loginForm" onSubmit={handleLogin}>
-        <label htmlFor="username">Username</label>
-        <input
-          className="loginInput"
-          placeholder="username"
-          id="username"
-          autoComplete="false"
-          ref={userRef}
-          type="text"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          className="loginInput"
-          placeholder="password"
-          id="password"
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <button>Log in!</button>
-      </form>
-    </section>
-     )}
-     </>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <form className="loginForm" onSubmit={handleLogin}>
+            <label htmlFor="user">Username</label>
+            <input
+              className="loginInput"
+              placeholder="user"
+              id="usern"
+              autoComplete="false"
+              ref={userRef}
+              type="text"
+              value={user}
+              onChange={(e) => {
+                setUser(e.target.value);
+              }}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              className="loginInput"
+              placeholder="pwd"
+              id="pwd"
+              type="password"
+              value={pwd}
+              onChange={(e) => {
+                setPwd(e.target.value);
+              }}
+            />
+            <button>Log in!</button>
+          </form>
+        </section>
+      )}
+    </>
   );
 }
